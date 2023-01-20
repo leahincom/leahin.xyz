d5027cdbff71a05c97fe06bf011c732ec24ada25
Lea.lah <leahincom@gmail.com>
2023-01-21 00:29:14 +0900

    :heavy_plus_sign: configure gtm-plugin

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,27 +1,35 @@
 import type { GatsbyConfig } from "gatsby";
+import "dotenv/config"
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `leahin.blog`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-vanilla-extract", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: ["gatsby-plugin-vanilla-extract", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-image", "gatsby-plugin-sitemap", 
+  {
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: process.env.GTM_CONTAINER_ID,
+      enableWebVitalsTracking: true,
+    }
+  },
+  {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  },
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }, {
+  },
+  {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
