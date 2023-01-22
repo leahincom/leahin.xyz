0db7732576b6125bdf00bacc025bd16a374c2033
Lea.lah <leahincom@gmail.com>
2023-01-22 21:24:19 +0900

    :package: test build artifacts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import type { PageProps } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 
 const IndexPage: React.FC<PageProps> = () => {
-  return <main>hello world</main>;
+  const data = useStaticQuery(graphql`
+    query IndexPage {
+      allMongodbBlogPosts {
+        edges {
+          node {
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  return <main>{data.allMongodbBlogPosts.edges[0]?.node.title}</main>;
 };
 
 export default IndexPage;
